7c64e4d38b5fdc127557a5913609129368c168d8
Marco <marco.mojica@hotmail.de>
2016-10-22 12:10:54 +0200

    funktion zum messen der geschwindikeit hinzugefügt und zum speichern der werte

--- nvis_node_snmp/app.js
+++ nvis_node_snmp/app.js
@@ -9,8 +9,13 @@ var udpSocket = dgram.createSocket("udp4");
 //zeitintervall in dem die OIDs abgefragt werden
 var interval = 10;
 
+// Länge des betrachteten zeitfensters in minuten
+var window = 1;
+
 //intervall für das regelmäsige abfragen der oids
 setInterval(() => {
+
+	console.log("reading values");
 
 	ports.forEach(getValues);
 
@@ -53,11 +58,20 @@ function getValues(port, index, portAry) {
 					console.error(err);
 			});
 
+
+			// überprüfe die Anzahl der gespeicherten messpunkte
+			// sind mehr als nötig vorhanden lösche das erste
+			if(port.maxSpeed.length > ((window * 60) / interval))
+				port.maxSpeed.shift();
+
+			// füge den aktuellen wert hinzu
+			port.maxSpeed.push(bitPerSec(port.lastIfOutOctets, varbinds[2].value));
+
+
 			// speichern der letzten in und out octets zum errechnen des Deltas
 			portAry[index].lastSpeed = varbinds[0].value;
 			portAry[index].lastIfInOctets = varbinds[1].value;
 			portAry[index].lastIfOutOctets = varbinds[2].value;
-
 		}
 
 	});
@@ -82,6 +96,12 @@ function utili(lastVal, currentVal, lastSpeed, currentSpeed) {
 }
 
 //geschwindigkeit in bit/s
-function bitPerSec() {
+function bitPerSec(lastVal, currentVal) {
+
+	// delta Berechnen
+	var delta = lastVal < currentVal ? (currentVal - lastVal) : (2 ^ 32 - 1 - lastVal + currentVal);
+
+	// bit/s berechnen und zurück geben
+	return Math.round((delta * 8 ) / interval);
 
 }
