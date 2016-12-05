379159db83f20a4bc33b98dc9a4b634338a37462
Marco <marco.mojica@hotmail.de>
2016-12-05 23:08:02 +0100

    einfache und simples interface gebaut um SNMP sammlung zu starten und zu stopen.
    ip:8080/nvis/start -> startet
    ip:8080/nvis/stop-> stopt

--- nvis_node_snmp/app.js
+++ nvis_node_snmp/app.js
@@ -1,7 +1,45 @@
 //benötigte Module
-var snmpCollector = require("./snmp_collector.js");
+const snmpCollector = require("./snmp_collector.js");
+const express = require("express");
+const app = express();
 
-snmpCollector.start();
+//port für den Webserver
+var port = process.env.PORT || 8080;
+
+//router für API calls
+var router = express.Router();
+
+// grüße an den besucher
+router.get('/', (req, res) => {
+	res.send("Hallo!");
+});
+
+//starte das SNMP sammeln
+router.get('/start', (req, res) => {
+	snmpCollector.start();
+	res.send("Start!");
+});
+
+//stoppe das SNMP sammeln
+router.get('/stop', (req, res) => {
+	snmpCollector.stop();
+	res.send("Stop!");
+});
+
+
+
+
+app.use('/nvis', router);
+
+app.listen(port, () => {
+	console.log('app listening on port ' + port);
+});
+
+
+
+
+
+//snmpCollector.start();
 
 setTimeout(() => {
 	//snmpCollector.stop();
