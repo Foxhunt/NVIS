1a38bb5dc10ba4f20f7269ff9d8d7c2c8a26637c
Marco <marco.mojica@hotmail.de>
2016-12-09 15:50:40 +0100

    Web Api eingebaut

--- nvis_node_snmp/app.js
+++ nvis_node_snmp/app.js
@@ -11,30 +11,31 @@ var router = express.Router();
 
 // grüße an den besucher
 router.get('/', (req, res) => {
-	res.send("Hallo! <br> <a href=\"/nvis/start\">Start</a> <br> <a href=\"/nvis/stop\">Stop</a>");
+	res.sendFile(__dirname + "/public/index.html");
 });
 
 //starte das SNMP sammeln
-router.get('/start', (req, res) => {
+router.post('/start', (req, res) => {
 
 	snmpCollector.start();
 
-	res.send("Started! <br> <a href=\"stop\">Stop</a> again!");
+	res.redirect("/");
 });
 
 //stoppe das SNMP sammeln
-router.get('/stop', (req, res) => {
+router.post('/stop', (req, res) => {
 
 	snmpCollector.stop();
 
-	res.send("Stoped! <br> <a href=\"start\">Start</a> again!");
+	res.redirect("/");
 });
 
 //app router benutzen lassen
+app.use(express.static('public'));
 app.use('/nvis', router);
-
 
 //app auf port lauschen lassen
 app.listen(port, () => {
 	console.log('app listening on port ' + port);
+	snmpCollector.start();
 });
